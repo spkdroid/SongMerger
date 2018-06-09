aeac77a322254ec0de699f0973671ecb7c273161
jeyabalaji <jeyabalaji.s@dal.ca>
2018-06-09 13:46:42 -0300

    pulled code from activity to model

--- app/src/main/java/com/dija/songmerge/songmerger/SelectSongActivity.java
+++ app/src/main/java/com/dija/songmerge/songmerger/SelectSongActivity.java
@@ -1,9 +1,6 @@
 package com.dija.songmerge.songmerger;
 
-import android.database.Cursor;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
@@ -12,16 +9,14 @@ import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
 import android.widget.EditText;
-import android.widget.Toast;
 
+import com.dija.songmerge.songmerger.Model.SongModel;
 import com.dija.songmerge.songmerger.adapter.SongAdapter;
 import com.dija.songmerge.songmerger.adapter.SongSelectedListener;
 import com.dija.songmerge.songmerger.helper.SongList;
-import com.dija.songmerge.songmerger.repository.SongRepository;
 import com.dija.songmerge.songmerger.repository.database.Song;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 public class SelectSongActivity extends AppCompatActivity implements TextWatcher, SongSelectedListener
@@ -29,7 +24,7 @@ public class SelectSongActivity extends AppCompatActivity implements TextWatcher
 
     private List<SongList> songList = new ArrayList<>();
     private List<SongList> resultList = new ArrayList<>();
-    private SongRepository repository;
+    private SongModel model;
     private RecyclerView recyclerView;
     private SongAdapter mAdapter;
 
@@ -55,52 +50,15 @@ public class SelectSongActivity extends AppCompatActivity implements TextWatcher
         recyclerView.setLayoutManager(llm);
         recyclerView.setHasFixedSize(true);
 
-        repository = new SongRepository(getApplicationContext());
+        model = new SongModel(getApplicationContext());
         prepareSongData();
     }
 
     private void prepareSongData() {
-
-        ArrayList<HashMap<String, String>> hello =  getAudioList();
-
-
-        for (HashMap<String,String> p:hello)
-        {
-            System.out.println(p.get("songTitle")+" "+p.get("songPath"));
-            SongList a = new SongList(p.get("songTitle"),p.get("songPath"));
-            resultList.add(a);
-        }
-
-        songList.addAll(resultList);
+        songList.addAll(model.getSongList(getContentResolver()));
         mAdapter.notifyDataSetChanged();
     }
 
-    public ArrayList<HashMap<String, String>> getAudioList() {
-
-        ArrayList<HashMap<String, String>> mSongsList = new ArrayList<HashMap<String, String>>();
-        Cursor mCursor = getContentResolver().query(
-                MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
-                new String[] { MediaStore.Audio.Media.DISPLAY_NAME,
-                        MediaStore.Audio.Media.DATA }, null, null, null);
-
-        int count = mCursor.getCount();
-        System.out.println("total no of songs are=" + count);
-        HashMap<String, String> songMap;
-
-        while (mCursor.moveToNext()) {
-            songMap = new HashMap<String, String>();
-            songMap.put(
-                    "songTitle",
-                    mCursor.getString(mCursor
-                            .getColumnIndexOrThrow(MediaStore.Audio.Media.DISPLAY_NAME)));
-            songMap.put("songPath", mCursor.getString(mCursor
-                    .getColumnIndexOrThrow(MediaStore.Audio.Media.DATA)));
-            mSongsList.add(songMap);
-        }
-
-        mCursor.close();
-        return mSongsList;
-    }
 
     @Override
     public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -130,28 +88,9 @@ public class SelectSongActivity extends AppCompatActivity implements TextWatcher
     public void onSongSelected(String songId, String songLocation)
     {
         Song songToBeAdded = new Song(songId, songLocation);
-        AddSongToRepo addSongToRepo = new AddSongToRepo();
-        addSongToRepo.execute(songToBeAdded);
+        model.addSongToRepo(songToBeAdded);
         Log.d("dbMessage","added");
     }
 
-    private class FetchSongFromRepo extends AsyncTask<String,Void,List<Song>> {
-        @Override
-        protected List<Song> doInBackground(String... params)
-        {
-            return repository.getAllSongs();
-        }
-    }
 
-    private class AddSongToRepo extends AsyncTask<Song,String, String> {
-
-        @Override
-        protected String doInBackground(Song... params)
-        {
-            repository.addSong(params[0]);
-            List<Song> songs = repository.getAllSongs();
-            Log.d("DbSize", String.valueOf(songs.size()));
-            return "adding song";
-        }
-    }
 }
