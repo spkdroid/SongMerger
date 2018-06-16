f7211605f073680f3396570636809797f713070a
Ramkumar Velmurugan <vrk1990@outlook.com>
2018-06-16 12:13:28 -0300

    Fixed FAB function

--- app/src/main/java/com/dija/songmerge/songmerger/MainActivity.java
+++ app/src/main/java/com/dija/songmerge/songmerger/MainActivity.java
@@ -68,8 +68,20 @@ public class MainActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
+                new AsyncTask<Void, Void, Integer>() {
+                    @Override
+                    protected Integer doInBackground(Void... voids) {
+                        songRepository.clearTable();
+                        return 0;
+                    }
+                    @Override
+                    protected void onPostExecute(Integer integer) {
+                        super.onPostExecute(integer);
+                        recreate();
+                    }
+                }.execute();
+
             }
         });
 
